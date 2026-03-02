db6244c832da9267fa893a8b4f3c20b585a1d85a
leoelguti <leonardogutierrezdev@hotmail.com>
2026-03-02 19:53:48 -0400

    feat: make server port configurable via environment variable

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ const { Server } = require('socket.io');
 const app = express();
 const server = http.createServer(app);
 const io = new Server(server);
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
 // Ruta de la carpeta donde se guarda la partida
 const savePath = path.join(__dirname, 'saved_games');
